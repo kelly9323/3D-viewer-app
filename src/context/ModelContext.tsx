b72635a5da4a1bfdab72b532cd12c5dacc6650da
ashlapak-cds <anastasiia@cognitive-design-systems.com>
2024-12-11 16:49:23 +0100

    add basic functionalities

--- src/context/ModelContext.tsx
+++ src/context/ModelContext.tsx
@@ -1,21 +1,57 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext } from "react";
+import * as THREE from "three";
+
 //centralize states management without passing props between compnents
 interface ModelContextProps {
   modelUrl: string | null;
   fileExtension: string | null;
+  showWireframe: boolean | undefined;
+  showAxesHelper: boolean | undefined;
+  modelMetadata: Record<string, any> | null;
+  localPlane: THREE.Plane | null;
+  globalPlane: THREE.Plane | null;
+
   setModelUrl: (url: string | null) => void;
   setFileExtension: (extension: string | null) => void;
+  setShowWireframe: (value: boolean) => void;
+  setShowAxesHelper: (value: boolean) => void;
+  setModelMetadata: (value: Record<string, any>) => void;
+  setLocalPlane: (plane: THREE.Plane) => void;
+  setGlobalPlane: (plane: THREE.Plane) => void;
 }
 
 const ModelContext = createContext<ModelContextProps | undefined>(undefined);
 
-export const ModelProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+export const ModelProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   const [modelUrl, setModelUrl] = useState<string | null>(null);
   const [fileExtension, setFileExtension] = useState<string | null>(null);
-
-//makes state available for children wrapped inside
+  const [showWireframe, setShowWireframe] = useState<boolean>(false);
+  const [showAxesHelper, setShowAxesHelper] = useState<boolean>(false);
+  const [modelMetadata, setModelMetadata] = useState<Record<string, any> | null>(null);
+  const [localPlane, setLocalPlane] = useState<THREE.Plane | null>(null);
+  const [globalPlane, setGlobalPlane] = useState<THREE.Plane | null>(null);
+  //makes state available for children wrapped inside
   return (
-    <ModelContext.Provider value={{ modelUrl,fileExtension, setModelUrl,setFileExtension }}>
+    <ModelContext.Provider
+      value={{
+        modelUrl,
+        setModelUrl,
+        fileExtension,
+        setFileExtension,
+        showWireframe,
+        setShowWireframe,
+        showAxesHelper,
+        setShowAxesHelper,
+        modelMetadata,
+        setModelMetadata,
+        localPlane,
+        globalPlane,
+        setLocalPlane,
+        setGlobalPlane,
+      }}
+    >
       {children}
     </ModelContext.Provider>
   );
@@ -23,6 +59,6 @@ export const ModelProvider: React.FC<{ children: React.ReactNode }> = ({ childre
 
 export const useModelContext = () => {
   const context = useContext(ModelContext);
-  if (!context) throw new Error('useModelContext is used inside ModelProvider');
+  if (!context) throw new Error("useModelContext is used inside ModelProvider");
   return context;
 };
--- src/utils/threeHelpers.ts
+++ src/utils/threeHelpers.ts
@@ -1,48 +1,119 @@
 import * as THREE from "three";
-import { STLLoader } from "three/examples/jsm/Addons.js";
+import { STLLoader } from "three/examples/jsm/loaders/STLLoader.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader.js";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
 
-export const loadModel = (url: string, scene: THREE.Scene, extension: string) => {
-  const gltfLoader = new GLTFLoader();
-  const stlLoader = new STLLoader();
-  console.log("ext", extension)
-  switch (extension) {
-    case "stl":
-      stlLoader.load(
-        url,
-        (geometry) => {
-          const material = new THREE.MeshPhongMaterial( { color: 0xFFFFFF, specular: 0x494949, shininess: 200 } );
-					const mesh = new THREE.Mesh( geometry, material );
+export const loadModel = (
+  url: string,
+  scene: THREE.Scene,
+  extension: string
+): Promise<THREE.Object3D | undefined> => {
+  return new Promise((resolve, reject) => {
+    // this.removeAllMeshes();
+    const gltfLoader = new GLTFLoader();
+    const stlLoader = new STLLoader();
+    const fbxLoader = new FBXLoader();
+    const objLoader = new OBJLoader();
+    console.log("ext", extension);
+    switch (extension) {
+      case "stl":
+        stlLoader.load(
+          url,
+          (geometry: THREE.BufferGeometry) => {
+            const mesh = new THREE.Mesh(geometry);
+            scene.add(mesh);
+            resolve(mesh)
+          },
+          undefined, // onProgress
+          (error) => {
+            console.error("Error loading model:", error);
+          }
+        );
+        break;
+      case "glb":
+      case "gltf":
+        console.log("model glb");
+        gltfLoader.load(
+          url,
+          (gltf) => {
+            gltf.scene.scale.set(160, 160, 160);
+            scene.add(gltf.scene);
+            resolve(gltf.scene)
+          },
+          undefined, // onProgress
+          (error) => {
+            console.error("Error loading model:", error);
+          }
+        );
+        break;
+      case "fbx":
+        fbxLoader.load(url, (fbx) => {
+          scene.add(fbx);
+          resolve(fbx);
+        });
+        break;
+      case "obj":
+        objLoader.load(url, (obj) => {
+          scene.add(obj);
+          resolve(obj)
+        });
+        break;
+      default:
+        break;
+    }
+  });
+};
 
-          // mesh.position.set( 0, - 0.25, 0.6 );
-					// mesh.rotation.set( 0, - Math.PI / 2, 0 );
-					mesh.scale.set( 0.5, 0.5, 0.5 );
 
-					// mesh.castShadow = true;
-					// mesh.receiveShadow = true;
+export const getModelMetadata = (model: THREE.Object3D): Record<string, any> => {
+  const metadata: Record<string, any> = {
+    fileName: "Unknown",
+    fileExtension: "Unknown",
+    verticesCount: 0,
+    facesCount: 0,
+    meshesCount: 0,
+    materialsCount: 0,
+    boundingBox: { width: 0, height: 0, depth: 0 },
+  };
 
-          scene.add(mesh);
-        },
-        undefined, // onProgress
-        (error) => {
-          console.error("Error loading model:", error);
-        }
-      );
-      break;
-    case "glb":
-      console.log("model glb")
-      gltfLoader.load(
-        url,
-        (gltf) => {
-          scene.add(gltf.scene);
-        },
-        undefined, // onProgress
-        (error) => {
-          console.error("Error loading model:", error);
-        }
-      );
-      break;
-    default:
-      break;
-  }
+  let verticesCount = 0;
+  let facesCount = 0;
+  const meshes: THREE.Mesh[] = [];
+  const materialsSet: Set<THREE.Material> = new Set();
+
+  model.traverse((child) => {
+    if (child instanceof THREE.Mesh) {
+      meshes.push(child);
+      verticesCount += child.geometry.attributes.position.count;
+
+      if (child.geometry.index) {
+        facesCount += child.geometry.index.count / 3;
+      } else {
+        facesCount += verticesCount / 3;
+      }
+
+      if (Array.isArray(child.material)) {
+        child.material.forEach((mat) => materialsSet.add(mat));
+      } else {
+        materialsSet.add(child.material);
+      }
+    }
+  });
+
+  const boundingBox = new THREE.Box3().setFromObject(model);
+  const size = boundingBox.getSize(new THREE.Vector3());
+
+  return {
+    ...metadata,
+    verticesCount,
+    facesCount,
+    meshesCount: meshes.length,
+    materialsCount: materialsSet.size,
+    boundingBox: {
+      width: size.x,
+      height: size.y,
+      depth: size.z,
+    },
+  };
 };
